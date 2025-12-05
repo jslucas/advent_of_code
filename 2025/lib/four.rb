c410e2d29ccc80ab0c19c71f2a4989e77d1bd382
jslucas <jspencerlucas@gmail.com>
2025-12-05 16:38:00 -0500

    12/4 - 2

--- 2025/lib/four.rb
+++ 2025/lib/four.rb
@@ -11,6 +11,20 @@ module TwentyFive
         end
       end
 
+      remove_acessible(rolls)
+    end
+
+    def self.remove_acessible(rolls, total = 0)
+      to_remove = rolls.select { |row, col| accessible?(rolls, row, col) }
+
+      return total if to_remove.empty?
+
+      to_remove.each { |roll| rolls.delete(roll) }
+
+      remove_acessible(rolls, total + to_remove.length) # 😎
+    end
+
+    def self.accessible?(rolls, row, col)
       # rubocop:disable Layout/ExtraSpacing
       neighbors = [
         [-1, -1], [-1, 0], [-1, 1],
@@ -19,9 +33,7 @@ module TwentyFive
       ]
       # rubocop:enable Layout/ExtraSpacing
 
-      rolls.count do |row, col|
-        neighbors.count { |x, y| rolls.include?([row + x, col + y]) } < 4
-      end
+      neighbors.count { |x, y| rolls.include?([row + x, col + y]) } < 4
     end
 
     def self.test_input
