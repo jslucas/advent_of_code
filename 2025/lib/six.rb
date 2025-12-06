cf7b415aa5229877e6515e137dc26234727ccaaf
jslucas <jspencerlucas@gmail.com>
2025-12-06 12:03:56 -0500

    12/6 - 2

--- 2025/lib/six.rb
+++ 2025/lib/six.rb
@@ -3,30 +3,45 @@ module TwentyFive
     def self.solve(test = true)
       input = test ? test_input : FetchInput.call(6)
 
-      input
+      columns =
+        input
         .split("\n")
-        .map { |l| l.split(' ') }
-        .reduce([]) do |columns, line|
-          line.each_with_index do |val, i|
-            columns[i] = [] if columns[i].nil?
+        .map(&:chars)
+        .transpose
 
-            if %w[+ - *].include?(val)
-              columns[i] = columns[i].reduce(val.to_sym)
-            else
-              columns[i] << val.to_i
-            end
+      sum = 0
+      stack = []
+      op = nil
+
+      columns.each do |column|
+        if column.all? { |x| x == ' ' }
+          sum += stack.reduce(op)
+          stack = []
+          op = nil
+          next
+        end
+
+        val = ''
+
+        column.each_with_index do |char, i|
+          if %w[+ - *].include?(char)
+            op = char.to_sym
+          else
+            val << char
           end
 
-          columns
+          stack << val.to_i if i == column.length - 1
         end
-        .sum
+      end
+
+      sum += stack.reduce(op)
     end
 
     def self.test_input
       <<~INPUT
         123 328  51 64#{' '}
          45 64  387 23#{' '}
-          6 98  215 314#{'  '}
+          6 98  215 314
         *   +   *   +#{'  '}
       INPUT
     end
